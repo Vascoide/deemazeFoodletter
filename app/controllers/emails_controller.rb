cbf9c7983554ae19a27e5746f31b1e77d731d135
Vasco Almeida <vasco.amorim_11@hotmail.com>
2023-11-22 21:11:56 +0000

    create is working

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,24 @@
 class EmailsController < ApplicationController
+    def new
+        @email = Email.new
+    end
+
+    def list
+        @emails = Email.all
+    end
+
+    def create
+        @email = Email.new(email_params)
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_email
+      @email = Email.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def email_params
+      params.require(:email).permit(:email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "emails#new"
+
+  get '/emails', to: 'emails#list'
+  get '/emails/new', to: 'emails#new'
+  post '/emails', to: 'emails#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
